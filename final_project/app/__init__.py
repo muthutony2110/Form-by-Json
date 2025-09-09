74aebc8aa6a2c0b1e7711a148a8ca90271baae13
muthutony2110 <115693674+muthutony2110@users.noreply.github.com>
2025-09-09 16:05:41 +0530

    Delete final_project directory

--- final_project/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from app.routes.generate import generate_blueprint
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(generate_blueprint)
-    return app
